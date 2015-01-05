f1a01ba7244e2dcbb7702918818f331a2788e7ae
Rustem Muslimov <r.muslimov@gmail.com>
2015-01-05 01:46:46 -0800

    Fixes

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 # coding:utf-8
 
+import os
+
 # Path on disk where all images and videos live
-DATABASE_PHOTOS_PATH = '~/Google Drive/Photos'
-DATABASE_VIDEOS_PATH = '~/Google Drive/Videos'
+DATABASE_PHOTOS_PATH = os.path.expanduser('~/Google Drive/Photos')
+DATABASE_VIDEOS_PATH = os.path.expanduser('~/Google Drive/Videos')
 
 # Directory name where SD usually is added
 POSSIBLE_STORAGE_PATHS = ('CANON_DC', 'EOS_DIGITAL')
